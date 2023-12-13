3e3eefba1d095aafffd33b0c620e528c39c2c0af
HuangChingChieh <v8414260@hotmail.com>
2023-12-13 16:30:12 +0800

    feat: 404頁調整樣式跟判斷邏輯

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,19 +1,17 @@
 <template>
   <common-container class="error-404">
-    <div class="py-4">
-      <div class="text-center">
-        <div class="h1 font-weight-boldest text-muted">404</div>
-        <div class="my-4">
-          哎呀～你尋找的網頁不存在哦！<br />看看我們隨機幫你選出的一篇文章吧！<br />或是<nuxt-link
-            to="/"
-            >點這裡</nuxt-link
-          >回到首頁！
-        </div>
+    <div class="text-center">
+      <div class="h1 font-weight-boldest text-muted">404</div>
+      <div class="my-4">
+        哎呀～你尋找的網頁不存在哦！<br />看看我們隨機幫你選出的一篇文章吧！<br />或是<nuxt-link
+          to="/"
+          >點這裡</nuxt-link
+        >回到首頁！
       </div>
+    </div>
 
-      <div v-if="article">
-        <article-card :article="article" class="border"></article-card>
-      </div>
+    <div v-if="article">
+      <article-card :article="article" class="border"></article-card>
     </div>
   </common-container>
 </template>
@@ -30,18 +28,15 @@ export default {
     return { article: null }
   },
   async fetch() {
-    const { count } = this.$store.state.articlesMetadata
-    const randomIndex = Math.ceil(Math.random() * count)
-
     const articles = await this.$content('articles', { deep: true })
       .only(articleQueryAttrs.card)
-      .sortBy('updatedAt', 'desc')
-      .skip(randomIndex)
-      .limit(1)
       .fetch()
 
-    const article = articles[0]
-    if (article) this.article = article
+    if (Array.isArray(articles)) {
+      const randomIndex = Math.round(Math.random() * (articles.length - 1))
+      const article = articles[randomIndex]
+      if (article) this.article = article
+    }
   },
   head: {
     title: '哎呀～你尋找的網頁不存在哦！',
