ab0c3804eeb05c48e6b523da192e83aad509ce14
HuangChingChieh <v8414260@hotmail.com>
2024-12-02 05:49:02 +0000

    fix: 不小心寫到tailwindcss的寫法

--- components/aside/aside-container.vue
+++ components/aside/aside-container.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="shadow-sm rounded bg-foreground p-3 lg:p-4 border-top border-primary border-5"
+    class="shadow-sm rounded bg-foreground p-3 p-lg-4 border-top border-primary border-5"
   >
     <AsideTitle v-if="title">
       {{ title }}
