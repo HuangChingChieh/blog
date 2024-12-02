3077b74b4f35bb3b2121c4a4d0221813cdb738d2
HuangChingChieh <v8414260@hotmail.com>
2024-12-02 07:12:39 +0000

    feat: 滾動時header縮小

--- components/common/common-header.vue
+++ components/common/common-header.vue
@@ -1,34 +1,22 @@
 <template>
   <header
-    :class="$style.header"
-    class="border-bottom border-primary border-2 position-sticky shadow-sm bg-foreground"
+    :class="[$style.header, shrink ? $style.shrink : '']"
+    class="border-bottom border-primary border-2 position-fixed shadow-sm bg-foreground w-100"
   >
     <InterfaceContainer
-      :class="`py-${mobileBreakpoint}-3`"
+      :class="$style.container"
       class="d-flex flex-row align-items-center"
     >
-      <CommonIcon
-        bordered
-        :class="$style.logo"
-      />
+      <CommonIcon bordered :class="$style.logo" />
       <div class="ms-3 flex-grow-1">
-        <NuxtLink
-          class="fw-bold text-body d-block text-decoration-none"
-          to="/"
-        >
+        <NuxtLink class="fw-bold text-body d-block text-decoration-none" to="/">
           隨機手札
         </NuxtLink>
-        <small
-          class="text-muted d-none"
-          :class="`d-${mobileBreakpoint}-block`"
-        >雜七雜八之地</small>
+        <small class="text-muted" :class="$style.hint">雜七雜八之地</small>
       </div>
 
       <div class="d-flex align-items-center justify-content-end">
-        <HeaderIconCategories
-          icon-class="d-flex p-0"
-          show-text
-        />
+        <HeaderIconCategories icon-class="d-flex p-0" show-text />
         <HeaderIconToc icon-class="d-flex p-0" />
         <HeaderIconTheme icon-class="d-flex p-0" />
         <HeaderIconSearch icon-class="d-flex p-0" />
@@ -47,13 +35,22 @@ import HeaderIconCategories from '~/components/header/header-icon-categories.vue
 import HeaderIconTheme from '~/components/header/header-icon-theme.vue'
 
 import { mobileBreakpoint } from '~/assets/css/export.module.scss'
+
+defineProps({
+  /**
+   * 是否縮小，目前用於滾動時控制樣式。
+   */
+  shrink: {
+    type: Boolean,
+    default: false,
+  },
+})
 </script>
 
-<style
-  lang="scss"
-  module
->
-.header {
+<style lang="scss" module>
+$transition-funciton: ease;
+
+@mixin header-shrink {
   padding-top: 0.75rem;
   padding-bottom: 0.75rem;
   top: 0;
@@ -63,10 +60,31 @@ import { mobileBreakpoint } from '~/assets/css/export.module.scss'
     height: 2rem;
     width: 2rem;
   }
+
+  .container {
+    padding-top: 0;
+    padding-bottom: 0;
+  }
+
+  .hint {
+    display: none;
+  }
 }
 
-@media #{$break-mobile} {
-  .header {
+.logo {
+  transition: height 0.15s $transition-funciton, width 0.15 $transition-funciton;
+}
+
+.header,
+.container {
+  transition: padding-top 0.15s $transition-funciton,
+    padding-bottom 0.15s $transition-funciton;
+}
+
+.header {
+  @include header-shrink;
+
+  @media #{$break-mobile} {
     padding-top: 0;
     padding-bottom: 0;
 
@@ -74,6 +92,19 @@ import { mobileBreakpoint } from '~/assets/css/export.module.scss'
       height: 3rem;
       width: 3rem;
     }
+
+    .container {
+      padding-top: 1rem;
+      padding-bottom: 1rem;
+    }
+
+    .hint {
+      display: block;
+    }
+  }
+
+  &.shrink {
+    @include header-shrink;
   }
 }
 </style>
