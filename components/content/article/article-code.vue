21e912beb51b85d1f4537d33f4d25181ea13da38
HuangChingChieh <v8414260@hotmail.com>
2025-02-21 15:31:07 +0000

    fix: 行內程式碼太長會破版問題

--- components/content/article/article-code.vue
+++ components/content/article/article-code.vue
@@ -1,6 +1,6 @@
 <template>
   <code
-    class="font-mono text-sm bg-gray-500 text-white mx-1 px-1 inline-block text-nowrap rounded align-bottom"
+    class="font-mono text-sm bg-gray-500 text-white mx-1 px-1 inline rounded align-bottom"
     ><slot
   /></code>
 </template>
